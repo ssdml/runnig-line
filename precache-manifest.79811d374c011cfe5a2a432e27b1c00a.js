1225617bce7fa898d79044d85fcb5689e2e6f5af
SergeyS <ss_dml@list.ru>
2019-01-05 21:50:33 +0300

    Updates

---
+++ precache-manifest.79811d374c011cfe5a2a432e27b1c00a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f9667af48fd468b771",
+    "url": "/myapp/static/css/main.f5bf6ca6.chunk.css"
+  },
+  {
+    "revision": "b3f9667af48fd468b771",
+    "url": "/myapp/static/js/main.b3f9667a.chunk.js"
+  },
+  {
+    "revision": "a0f5e5c5459f64a53731",
+    "url": "/myapp/static/js/1.a0f5e5c5.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "3ff4e00e29185e613bda348926529298",
+    "url": "/myapp/index.html"
+  }
+];
